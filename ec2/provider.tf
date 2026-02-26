6e16f8dc611a34c2d9fc250cbf2e81d37105fcde
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-02-26 18:03:46 +0530

    ec2 terraform

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.32.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.32.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  # Configuration options
 }
