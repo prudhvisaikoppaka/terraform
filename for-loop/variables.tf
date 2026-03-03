a5d48db1ad478d07092543ba20eca77904b5cfff
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-03-03 17:33:54 +0530

    for-loop terraform

---
+++ for-loop/variables.tf
@@ -0,0 +1,66 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-0220d79f3f480ecf5"
+  description = "AMI ID of joindevops RHEL9"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name = "Roboshop"
+        Purpose = "variables-demo" # Can add n number of tags
+    }
+}
+
+variable "sg_name"{
+     default = "allow-all"
+}
+
+variable "sg_description" {
+    default = "allowing all ports from internet"
+}
+
+variable "from_port" {
+    default = 0
+}
+
+variable "to_port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+
+variable "environment" {
+    default = "prod"
+}
+
+variable "instances" {
+    default = {
+        mongodb = "t3.micro" # each keyword is assigned for every iteration. You will get each.key and each.value
+        redis = "t3.micro"
+        mysql = "t3.small"
+        rabbitmq = "t3.micro"
+    }
+}
+
+variable "zone_id" {
+    default = "Z08496491D9BJEMR7DC94"
+}
+
+variable "domain_name" {
+    default = "prudhvisai.space"
+}
