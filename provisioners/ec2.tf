47df043ee1918f2c2c954f4f5f1425c41d09a897
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-03-12 18:31:44 +0530

    terraform provisioners

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -7,6 +7,12 @@ resource "aws_instance" "roboshop" {
 
   provisioner "local-exec" {
     command = "echo ${self.private_ip} > inventory"
+    # on_failure = continue # ignoring errors
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'instance is destroyed' "
+    when = destroy
   }
 }
 
