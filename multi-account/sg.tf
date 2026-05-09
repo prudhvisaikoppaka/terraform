9eced88ce3ee3153f3a018821a171af0bf3e936a
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-09 21:40:57 +0530

    terraform infra aws multi-account

--- multi-account/sg.tf
+++ multi-account/sg.tf
@@ -24,28 +24,28 @@ resource "aws_security_group" "allow_all_dev" {
     provider = aws.dev
 }
 
-resource "aws_security_group" "allow_all_prod" {
-    name        = "allow_all_prod"
-    description = "allow all traffic"
+# resource "aws_security_group" "allow_all_prod" {
+#     name        = "allow_all_prod"
+#     description = "allow all traffic"
 
-    ingress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+#     ingress {
+#         from_port        = 0
+#         to_port          = 0
+#         protocol         = "-1"
+#         cidr_blocks      = ["0.0.0.0/0"]
+#         ipv6_cidr_blocks = ["::/0"]
+#     }
+#     egress {
+#         from_port        = 0
+#         to_port          = 0
+#         protocol         = "-1"
+#         cidr_blocks      = ["0.0.0.0/0"]
+#         ipv6_cidr_blocks = ["::/0"]
+#     }
 
-    tags = {
-        Name = "allow-all-prod"
-    }
+#     tags = {
+#         Name = "allow-all-prod"
+#     }
 
-    provider = aws.prod
-}
+#     provider = aws.prod
+# }
