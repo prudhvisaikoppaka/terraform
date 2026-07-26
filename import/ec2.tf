7ef579cf95a21308f45571fb4803b0046d4ce550
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-07-26 18:01:56 +0530

    terraform import

--- import/ec2.tf
+++ import/ec2.tf
@@ -1,3 +1,10 @@
-resource "aws_instance" "Linux" {
+resource "aws_instance" "linux" {
  
+ ami = "ami-0220d79f3f480ecf5"
+ instance_type = "t3.micro"
+ vpc_security_group_ids = ["sg-09aac8aee4b1881eb"]
+
+ tags = {
+    Name = "linux-change"
+ }
 }
